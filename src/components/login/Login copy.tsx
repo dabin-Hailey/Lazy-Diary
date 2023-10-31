8dac698037f36c84af5fd293809354d9fd310291
김다빈 <dabin@gimdabin-ui-MacBookAir.local>
2023-10-31 23:24:26 +0900

    Fix: 안 쓰는 값들 삭제

--- src/components/login/Login copy.tsx
+++ src/components/login/Login copy.tsx
@@ -6,7 +6,15 @@ import GoogleLogin from "./Google";
 
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { colors, loginTitle } from "../../styles";
+import {
+  colors,
+  loginBtn,
+  loginForm,
+  loginFormText,
+  loginInput,
+  loginTitle,
+  reverseBtn
+} from "../../styles";
 
 const Login: React.FC = () => {
   const navigate = useNavigate();
@@ -47,6 +55,30 @@ const Login: React.FC = () => {
         </p>
       </div>
       <h1 css={loginTitle}>로그인</h1>
+      <form css={loginForm}>
+        <p css={loginFormText}>이메일</p>
+        <input
+          css={loginInput}
+          type="text"
+          title="이메일을 입력해주세요."
+        />
+        <p css={loginFormText}>비밀번호</p>
+        <input
+          css={loginInput}
+          type="password"
+          title="비밀번호를 입력해주세요."
+        />
+        <button
+          type="submit"
+          css={loginBtn}
+        >
+          로그인
+        </button>
+      </form>
+      <button css={signUpBtn}>회원가입</button>
+      <div css={loginTextWrapper}>
+        <p css={loginText}>또는</p>
+      </div>
       <button
         css={kakaoLoginBtn}
         onClick={loginWithKakao}
@@ -94,6 +126,47 @@ const loginDesription = css`
   p {
     font-size: 1.8rem !important;
     line-height: 3rem;
+  }
+`;
+
+const signUpBtn = css`
+  ${reverseBtn} & {
+    width: 35rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
+    background-color: ${colors.primaryColor};
+    border: 2px solid ${colors.primaryColor};
+
+    :hover {
+      color: ${colors.primaryColor};
+      border: 2px solid ${colors.primaryColor};
+    }
+  }
+`;
+
+const loginTextWrapper = css`
+  width: 35rem;
+  margin: 2rem 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  ::before {
+    content: "";
+    position: relative;
+    top: 50%;
+    width: 43%;
+    transform: translateY(50%);
+    border-top: 1px solid rgba(95, 65, 65, 0.5);
+  }
+
+  ::after {
+    content: "";
+    position: relative;
+    top: 50%;
+    width: 43%;
+    transform: translateY(50%);
+    border-top: 1px solid rgba(95, 65, 65, 0.5);
   }
 `;
 
